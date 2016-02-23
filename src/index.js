3cdbf5219c59975b5f22d032b6cf8fc918ccbabc
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-02-23 17:56:46 +0800

    mount point made constant

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import { APP_MOUNT_POINT } from './constants/app';
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById(APP_MOUNT_POINT));
